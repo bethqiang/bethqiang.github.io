9031d73dc4e7fa7bff796ec79a32f13ddd5ca067
Beth Qiang <bethany.qiang@gmail.com>
2020-10-25 20:28:26 -0400

    add other pages but with coming soon msg

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+export default function Blog() {
+  return (
+    <Layout>
+      <main className="mainContent">
+        <h2>coming soon! stay tuned.</h2>
+      </main>
+    </Layout>
+  );
+}
